3375be4740dc943ca8eda5ce65ec35343fb3c59c
Teknon766 <honzaander@gmail.com>
2025-06-20 19:46:21 +0100

    add 8kyu kata: repeat string solution

---
+++ 8kyu/string_repeat.py
@@ -0,0 +1,13 @@
+'''
+Write a function that accepts a non-negative integer n and a string s as parameters,
+ and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+'''
+
+def repeat_str(n, s):
+    if n < 0:
+        raise ValueError("n must be non negative")
+    return s * n

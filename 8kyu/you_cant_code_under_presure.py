638d9f91ae148ade2541de8fac4d3f3f54dda3f5
Teknon766 <honzaander@gmail.com>
2025-06-21 10:28:30 +0100

    Add 8kyu kata(test data generator included): you_cant_code_under_presure.py

--- 8kyu/you_cant_code_under_presure.py
+++ 8kyu/you_cant_code_under_presure.py
@@ -1,6 +1,19 @@
+from random import randint
 '''
 Description:
 Code as fast as you can! You need to double the integer and return it.
 '''
 
 
+def double_integer(i):
+    return i*2
+
+
+#--------------------------test--------------------------
+
+
+def test_double_integer():
+    for loop in range(50):
+        feed = randint(-1000,1000)
+        expected = feed * 2
+        assert double_integer(feed) == expected

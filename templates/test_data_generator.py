da1db4e4bb4c01a3e1894ffe10ed4d87c47cb3f8
Teknon766 <honzaander@gmail.com>
2025-06-21 19:16:06 +0100

    Add template random test data generator will organize it later i will do 3 in total 1.string and number, 2.number. 3.string

---
+++ templates/test_data_generator.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+test_data = []
+for loop in range(5):  # 5 test cases
+    length = random.randint(4, 10)  # random length of each list
+    nums = []
+    total = 0
+    for loop in range(length):
+        n = random.randint(0, 10)
+        # Randomly decide: keep as int or convert to str
+        if random.choice([True, False]):
+            nums.append(n)
+        else:
+            nums.append(str(n))
+        total += n
+    test_data.append((nums, total))
+
+
+test_data = []
+for loop in range(10):  # 10 test cases
+    
+    for loop in range(10):
+        n = random.randint(0, 9)
+     # saving data in format i need for testing   
+    test_data.append((n , n))
+
+chars = string.ascii_letters + ' '
+feed = ''.join(random.choice(chars) for loop in range(length))

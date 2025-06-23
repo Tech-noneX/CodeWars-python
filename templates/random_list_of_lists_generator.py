ad50f340a10ff9fc2cee72d27ce44a99e49bf98c
Teknon766 <honzaander@gmail.com>
2025-06-23 23:26:03 +0100

    Add random_list_of_random_lists_generator.py(this program generates rundom lists of random lengths and random numbers)

---
+++ templates/random_list_of_lists_generator.py
@@ -0,0 +1,32 @@
+import random
+ # list of lists (random length, random numbers)
+random_lists = []
+ # addition of 2 min numbers from each lists in numbers
+sum_min = [] 
+ # number of lists in the list
+num_of_seq = 150
+for loop in range(num_of_seq):
+     # makes random length of lists
+    list_length = random.randint(5,10)
+    seq = []
+
+    for loop in range(list_length):
+         # makes random numbers in random lists and append them to list seq
+        seq.append(random.randint(-20,500))
+     # takes all lists cooked and append them to min one
+    random_lists.append(seq) 
+
+for seq in random_lists:
+     # temporary sort numbers in list to make them ready for addition 2 min num
+    sorted_seq = sorted(seq) 
+     # Calculate the sum of the two smallest numbers in each list 
+    min_pair_sum = sorted_seq[0] + sorted_seq[1]
+     # result is sent to list sum_min
+    sum_min.append(min_pair_sum)
+ # Pair input/output for automated test data
+test_data = [(feed,expected) for feed,expected in zip(random_lists,sum_min)]
+ # for overview to see if is going good
+print(seq)
+print(random_lists)
+print(sum_min)
+print(test_data)
